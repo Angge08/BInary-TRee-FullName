0afe70582d9a00845edda0a74b7467187a13413d
Angge08 <anggedelmendo08@gmail.com>
2023-01-15 12:15:44 +0800

    putting the condition of self.right and self.left in deletion

--- BinaryTree-FullName.py
+++ BinaryTree-FullName.py
@@ -106,3 +106,16 @@ class BinarySearchTreeNode:
 
         return self
 
+        def calculate_sum(self):
+            left_sum = self.left.calculate_sum() if self.left else 0
+            right_sum = self.right.calculate_sum() if self.right else 0
+            return self.data + left_sum + right_sum
+
+    def build_tree(elements):
+        root = BinarySearchTreeNode(elements[0])
+
+        for i in range(1, len(elements)):
+            root.add_child(elements[i])
+
+        return root
+
